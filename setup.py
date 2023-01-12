9374f0d7f5fc828a4c867a7f01918429bb4c46dd
Rafael Ciarlini <rafaelciarlini@gmail.com>
2023-01-12 01:05:36 -0300

    arrquivos setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cia_tools',   
+    version= "0.0.1",
+    description= "dev",
+
+    url='https://github.com/rafaelciarlini/cia_tools.git',
+    author='Rafael Ciarlini',
+    author_email='rafaelciarlini@gmail.com',
+
+    py_modules=['cia_tools'],
+)
